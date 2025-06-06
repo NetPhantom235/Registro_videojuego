afb24fbd9901e8c3dfc08b25da9735a2ba4aa767
JVARAS <ju.varasc@duocuc.cl>
2025-06-06 10:41:35 -0400

    Paso 2: Registro de videojuego

--- app.py
+++ app.py
@@ -17,7 +17,24 @@ while True:
     print("="*50)
     option=(input("ingresa una opcion (1-5): "))
     if option=="1":
-        pass
+        codigo=int(input("Ingresa el codigo del videojuego: "))
+        nombre= (input("ingresa el nombre del juego"))
+        genero=(input("ingresa el genero del videojuego: "))
+        print("\nPlataformas disponibles: ")
+        print("1. ps5") 
+        print("2. PC") 
+        print("3. xbox series x")
+        print("4. nintendo switch")
+        plataforma_codigo=int(input("Seleccione el numero de la plataforma: "))
+        plataforma=plataformas[plataforma_codigo - 1]
+        videojuegos={
+            "codigo":codigo,
+            "nombre":nombre,
+            "genero":genero,
+            "plataforma":plataforma
+        }
+        videojuegos.append(videojuegos)
+        print("Videojuego registrado con exito!")
     elif option=="2":
         pass
     elif option=="3":
